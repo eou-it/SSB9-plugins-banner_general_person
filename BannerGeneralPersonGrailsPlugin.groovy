f8742555166de1c32468c31d437d667e13020f5b
manjunathadg <Manjunatha.DG@sungard.com>
2011-08-10 11:55:34 +0530

    Updated the version number.

--- BannerGeneralPersonGrailsPlugin.groovy
+++ BannerGeneralPersonGrailsPlugin.groovy
@@ -50,7 +50,7 @@ class BannerGeneralPersonGrailsPlugin {
     // more control on 'when' a grails app is updated to use a newer plugin version, and therefore 'could' allow delayed testing within those apps
     // independent of deploying a new plugin build to Nexus.
     //
-    String version = "0.0.6"
+    String version = "0.0.7"
 //    String version = "0.1.0"
 
     // the version or versions of Grails the plugin is designed for
