ee5b8530d2c7303e0106dbc9546e9ea1eaac60ff
suresh.babu <suresh.babu@sungard.com>
2011-12-21 14:14:28 +0530

    Fix for CSAT-73

--- grails-app/domain/com/sungardhe/banner/general/person/PersonIdentificationName.groovy
+++ grails-app/domain/com/sungardhe/banner/general/person/PersonIdentificationName.groovy
@@ -1,12 +1,12 @@
 /*********************************************************************************
  Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
- This copyrighted software contains confidential and proprietary information of 
- SunGard Higher Education and its subsidiaries. Any use of this software is limited 
- solely to SunGard Higher Education licensees, and is further subject to the terms 
- and conditions of one or more written license agreements between SunGard Higher 
+ This copyrighted software contains confidential and proprietary information of
+ SunGard Higher Education and its subsidiaries. Any use of this software is limited
+ solely to SunGard Higher Education licensees, and is further subject to the terms
+ and conditions of one or more written license agreements between SunGard Higher
  Education and the licensee in question. SunGard is either a registered trademark or
  trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
+ Banner and Luminis are either registered trademarks or trademarks of SunGard Higher
  Education in the U.S.A. and/or other regions and/or countries.
  **********************************************************************************/
 package com.sungardhe.banner.general.person
@@ -431,9 +431,7 @@ class PersonIdentificationName implements Serializable {
     }
     //Used for Lookup.
     public static def fetchBySomeBannerId() {
-        def filter = null
-        def returnObj = [list: PersonIdentificationName.fetchByBannerId(filter)]
-        return returnObj
+        return [list: []]
     }
 
     //Used for Lookup.
@@ -446,9 +444,7 @@ class PersonIdentificationName implements Serializable {
 
     //Used for Lookup.
     public static def fetchBySomeName() {
-        def filter = null
-        def returnObj = [list: PersonIdentificationName.fetchByName(filter)]
-        return returnObj
+        return [list: []]
     }
 
     //Used for Lookup.
