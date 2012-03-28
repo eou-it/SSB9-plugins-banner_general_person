0b20a0dfc75cf408265f4ae5b8f75c675f5146f0
Raji Venkatesan <raji.venkatesan@sungardhe.com>
2012-03-28 12:09:20 -0400

    Added PersonRelatedHoldService

---
+++ grails-app/services/com/sungardhe/banner/general/person/PersonRelatedHoldService.groovy
@@ -0,0 +1,77 @@
+
+/*******************************************************************************
+ Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
+
+ This copyrighted software contains confidential and proprietary information of 
+ SunGard Higher Education and its subsidiaries. Any use of this software is limited 
+ solely to SunGard Higher Education licensees, and is further subject to the terms 
+ and conditions of one or more written license agreements between SunGard Higher 
+ Education and the licensee in question. SunGard is either a registered trademark or
+ trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
+ Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
+ Education in the U.S.A. and/or other regions and/or countries.
+ *******************************************************************************/
+/**
+ Banner Automator Version: 1.29
+ Generated: Thu Mar 08 11:17:12 EST 2012 
+ */
+package com.sungardhe.banner.general.person
+
+import com.sungardhe.banner.exceptions.ApplicationException
+import com.sungardhe.banner.service.ServiceBase
+import org.springframework.security.core.context.SecurityContextHolder
+
+// NOTE:
+// This service is injected with create, update, and delete methods that may throw runtime exceptions (listed below).  
+// These exceptions must be caught and handled by the controller using this service.
+// 
+// update and delete may throw com.sungardhe.banner.exceptions.NotFoundException if the entity cannot be found in the database
+// update and delete may throw org.springframework.orm.hibernate3.HibernateOptimisticLockingFailureException a runtime exception if an optimistic lock failure occurs
+// create, update, and delete may throw grails.validation.ValidationException a runtime exception when there is a validation failure
+
+class PersonRelatedHoldService extends ServiceBase {
+
+    boolean transactional = true
+
+    def sessionFactory
+
+    void preUpdate (map) {
+          validateHoldForUpdate(map.domainModel)
+    }
+
+
+    private validateHoldForUpdate(PersonRelatedHold hold) {
+        if (!(hold.userData == SecurityContextHolder.context?.authentication?.principal?.username)) {
+            if (findDirty(hold,'holdType'))
+                throw new ApplicationException(PesonRelatedHold, "@@r1:invalidHoldUser")
+            if (findDirty(hold,'releaseIndicator'))
+          //  if (hold.isDirty('releaseIndicator'))
+                    throw new ApplicationException(PersonRelatedHold, "@@r1:invalidHoldUser")
+            if (hold.releaseIndicator) {
+                if (findDirty(hold,'reason'))
+                    throw new ApplicationException(PersonRelatedHold, "@@r1:invalidHoldUser")
+                if (findDirty(hold,'fromDate'))
+                    throw new ApplicationException(PersonRelatedHold, "@@r1:invalidHoldUser")
+                if (findDirty(hold,'toDate'))
+                    throw new ApplicationException(PersonRelatedHold, "@@r1:invalidHoldUser")
+            }
+    }
+
+    }
+
+    private findDirty(hold, String field) {
+        def content = ServiceBase.extractParams(PersonRelatedHold, hold)
+        def domainObject = PersonRelatedHold.get(content?.id)
+        domainObject.properties = content
+        def changedNames = domainObject.dirtyPropertyNames
+        def changed = changedNames.find { it == field}
+        return changed       
+    }
+
+    /**
+     * Please put all the custom methods in this protected section to protect the code
+     * from being overwritten on re-generation
+     */
+    /*PROTECTED REGION ID(personrelatedhold_custom_service_methods) ENABLED START*/
+    /*PROTECTED REGION END*/
+}
---
+++ test/integration/com/sungardhe/banner/general/person/PersonRelatedHoldServiceIntegrationTests.groovy
@@ -0,0 +1,156 @@
+
+/*********************************************************************************
+ Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
+ This copyrighted software contains confidential and proprietary information of 
+ SunGard Higher Education and its subsidiaries. Any use of this software is limited 
+ solely to SunGard Higher Education licensees, and is further subject to the terms 
+ and conditions of one or more written license agreements between SunGard Higher 
+ Education and the licensee in question. SunGard is either a registered trademark or
+ trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
+ Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
+ Education in the U.S.A. and/or other regions and/or countries.
+ **********************************************************************************/
+ 
+package com.sungardhe.banner.general.person
+
+import java.text.SimpleDateFormat
+
+import com.sungardhe.banner.general.system.HoldType
+import com.sungardhe.banner.general.system.Originator
+import com.sungardhe.banner.testing.BaseIntegrationTestCase
+import groovy.sql.Sql
+import org.springframework.orm.hibernate3.HibernateOptimisticLockingFailureException
+import com.sungardhe.banner.exceptions.ApplicationException
+
+class PersonRelatedHoldServiceIntegrationTests extends BaseIntegrationTestCase {
+
+	def personRelatedHoldService
+	
+	protected void setUp() {
+		formContext = ['SOAHOLD']
+		super.setUp()
+	}
+
+	protected void tearDown() {
+		super.tearDown()
+	}
+
+	void testCreateAndUpdatePersonRelatedHold() {
+		def personRelatedHold = newPersonRelatedHold()
+        def map = [domainModel: personRelatedHold]
+		personRelatedHold = personRelatedHoldService.create(map)
+		//Test if the generated entity now has an id assigned		
+        assertNotNull personRelatedHold.id
+        assertEquals "Hold Type not as expected",personRelatedHold.holdType.code,"RG"
+        assertEquals "Originator not as expected",personRelatedHold.originator.code,"ACCT"
+        personRelatedHold.reason = "YYYY"
+        personRelatedHold = personRelatedHoldService.update([domainModel:personRelatedHold])
+        assertEquals "Reason not as expected","YYYY",personRelatedHold.reason
+         println(' the version before is ' + personRelatedHold.version)
+        def sql
+        try {
+            sql = new Sql(sessionFactory.getCurrentSession().connection())
+            sql.executeUpdate("update SPRHOLD set SPRHOLD_USER = 'systest1' where SPRHOLD_SURROGATE_ID = ?",[personRelatedHold.id])
+        } finally{
+            sql?.close()
+        }
+        personRelatedHold.discard()
+        try {
+            personRelatedHold = PersonRelatedHold.findById(personRelatedHold.id)
+            personRelatedHold.releaseIndicator = true
+            personRelatedHoldService.update([domainModel:personRelatedHold])
+            fail("this should have failed, user doing the update is not the user who last udated the hold")
+        }
+        catch (ApplicationException ae){
+            assertApplicationException ae,"invalidHoldUser"
+        }
+         try {
+            sql = new Sql(sessionFactory.getCurrentSession().connection())
+            sql.executeUpdate("update SPRHOLD set SPRHOLD_USER = 'grails_user' where SPRHOLD_SURROGATE_ID = ?",[personRelatedHold.id])
+        } finally{
+            sql?.close()
+        }
+        personRelatedHold.discard()
+        personRelatedHold = PersonRelatedHold.findById(personRelatedHold.id)
+        personRelatedHold.releaseIndicator = true
+        personRelatedHold = personRelatedHoldService.update([domainModel:personRelatedHold])
+        assertEquals "Release indicator not as expected",true,personRelatedHold.releaseIndicator
+        
+        try {
+            sql = new Sql(sessionFactory.getCurrentSession().connection())
+            sql.executeUpdate("update SPRHOLD set SPRHOLD_USER = 'systest1' where SPRHOLD_SURROGATE_ID = ?",[personRelatedHold.id])
+        } finally{
+            sql?.close()
+        }
+        personRelatedHold.discard()   
+        try {
+            personRelatedHold = PersonRelatedHold.findById(personRelatedHold.id)
+            personRelatedHold.reason = "XXXX"
+            personRelatedHoldService.update([domainModel:personRelatedHold])
+            fail("this should have failed, user doing the update is not the user who last udated the hold")
+        }
+        catch (ApplicationException ae){
+            assertApplicationException ae,"invalidHoldUser"
+        }
+         personRelatedHold.discard()
+         try {
+            personRelatedHold = PersonRelatedHold.findById(personRelatedHold.id)
+            personRelatedHold.fromDate = new Date()
+            personRelatedHoldService.update([domainModel:personRelatedHold])
+            fail("this should have failed, user doing the update is not the user who last udated the hold")
+        }
+        catch (ApplicationException ae){
+            assertApplicationException ae,"invalidHoldUser"
+        }
+         try {
+            sql = new Sql(sessionFactory.getCurrentSession().connection())
+            sql.executeUpdate("update SPRHOLD set SPRHOLD_USER = 'grails_user' where SPRHOLD_SURROGATE_ID = ?",[personRelatedHold.id])
+        } finally{
+            sql?.close()
+        }
+        personRelatedHold.discard()
+        personRelatedHold = PersonRelatedHold.findById(personRelatedHold.id)
+        personRelatedHold.reason = "XXXX"
+        personRelatedHold = personRelatedHoldService.update([domainModel:personRelatedHold])
+        assertEquals "Reason not as expected","XXXX",personRelatedHold.reason
+
+	}
+
+  
+    
+  private def newPersonRelatedHold() {
+  
+    /**
+     * Please use the appropriate finder methods to load the references here
+     * This area is being protected to preserve the customization on regeneration 
+     */
+    /*PROTECTED REGION ID(personrelatedhold_integration_tests_data_fetch_for_references) ENABLED START*/
+	    def iholdType = HoldType.findWhere(code: "RG")
+	    def ioriginator = Originator.findWhere(code: "ACCT")
+        def df1 = new SimpleDateFormat("yyyy-MM-dd")
+        def fromDate = df1.parse("2010-07-01")
+        def toDate = df1.parse("2010-09-01")
+        def pidm = PersonUtility.getPerson("HOS00001").pidm
+
+    /*PROTECTED REGION END*/
+     
+    def personRelatedHold = new PersonRelatedHold(
+    		pidm: pidm,
+    		userData: "TTTTT", 
+            fromDate: fromDate,
+            toDate: toDate,
+    		releaseIndicator: false,
+    		reason: "TTTTT", 
+    		amountOwed: 1,
+			holdType: iholdType, 
+			originator: ioriginator, 
+            lastModified: new Date(),
+			lastModifiedBy: "test", 
+			dataOrigin: "Banner"
+        )
+        return personRelatedHold
+    }
+
+   
+    /*PROTECTED REGION END*/
+}
