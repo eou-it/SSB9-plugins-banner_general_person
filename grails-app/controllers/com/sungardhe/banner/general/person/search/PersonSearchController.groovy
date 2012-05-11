a8e3092e3e7741f4593fa9670cb5a481dc744912
Michael Hitrik <michael.hitrik@sungardhe.com>
2012-05-11 09:13:53 -0400

    Added test for JSON returned

--- grails-app/controllers/com/sungardhe/banner/general/person/search/PersonSearchController.groovy
+++ grails-app/controllers/com/sungardhe/banner/general/person/search/PersonSearchController.groovy
@@ -45,4 +45,48 @@ class PersonSearchController {
 
         render people as JSON
     }
+
+    //This is a test action
+    def searchPersonTest = {
+
+        def pageOffset = params.int('offset') ? params?.int('offset') : 0
+        def pageMaxSize = params.int('max') ? params?.int('max') : 100
+
+        def filterData = [:]
+        def param = [:]
+        filterData.params = param
+
+        def x = []
+
+        if (params.city) {
+            param."city" = "%"+params.city+"%"
+
+            def m1 = [:]
+            m1."key" = "city"
+            m1."binding" = "city"
+            m1."operator" = "contains"
+            x.add(m1)
+        }
+
+        if (params.zip) {
+            param."zip" = params.zip
+
+            def m2 = [:]
+            m2."key" = "zip"
+            m2."binding" = "zip"
+            m2."operator" = "contains"
+            x.add(m2)
+        }
+
+        if (x.size() > 0) {
+            filterData.criteria = x
+        }
+
+        params.pagingAndSortParams = ["max": pageMaxSize, "offset": pageOffset]
+
+        def result = personSearchService.personSearch(params?.searchFilter, filterData, params.pagingAndSortParams)
+        def people = [people: result]
+
+        render people as JSON
+    }
 }
