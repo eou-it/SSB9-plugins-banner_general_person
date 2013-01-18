b8d018dfce2de33ae83074cf5ce9795058807a05
sridhanya <sridhanya.ganapathi@ellucian.com>
2013-01-18 19:02:49 +0530

    Fixing the failing tests

--- test/integration/net/hedtech/banner/general/person/PersonIdentificationNameIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/person/PersonIdentificationNameIntegrationTests.groovy
@@ -1,12 +1,12 @@
 /** *******************************************************************************
  Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
- This copyrighted software contains confidential and proprietary information of 
- SunGard Higher Education and its subsidiaries. Any use of this software is limited 
- solely to SunGard Higher Education licensees, and is further subject to the terms 
- and conditions of one or more written license agreements between SunGard Higher 
+ This copyrighted software contains confidential and proprietary information of
+ SunGard Higher Education and its subsidiaries. Any use of this software is limited
+ solely to SunGard Higher Education licensees, and is further subject to the terms
+ and conditions of one or more written license agreements between SunGard Higher
  Education and the licensee in question. SunGard is either a registered trademark or
  trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
+ Banner and Luminis are either registered trademarks or trademarks of SunGard Higher
  Education in the U.S.A. and/or other regions and/or countries.
  ********************************************************************************* */
 
@@ -22,7 +22,7 @@ class PersonIdentificationNameIntegrationTests extends BaseIntegrationTestCase {
 
 
     protected void setUp() {
-        formContext = ['SPAIDEN'] // Since we are not testing a controller, we need to explicitly set this
+        formContext = ['SOAIDEN'] // Since we are not testing a controller, we need to explicitly set this
         super.setUp()
     }
 
