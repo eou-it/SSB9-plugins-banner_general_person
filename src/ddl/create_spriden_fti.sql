4d20bdc269579fa193a63c93cc7e3da689cb1daf
Margy Hockett <margy.hockett@sungardhe.com>
2013-07-25 13:31:44 -0400

    add whenever clause to new index create and drop sql

--- src/ddl/create_spriden_fti.sql
+++ src/ddl/create_spriden_fti.sql
@@ -17,21 +17,30 @@ REM
 --
 -- Drop the stoplist for name search.
 --
+whenever sqlerror continue;
 begin
-  ctx_ddl.drop_stoplist('STOPLIST_NAMESEARCH');
+  ctx_ddl.drop_stoplist('STOPLIST_NAMESEARCH'); 
+exception
+	when others then null;
 end;
 /
+whenever sqlerror exit rollback;
 --
 -- Create the stoplist for name search.
 --
+whenever sqlerror continue;
 begin
      ctx_ddl.create_stoplist('STOPLIST_NAMESEARCH');
+exception
+		when others then null;
 end;
 /
+whenever sqlerror exit rollback;
 
 --
 -- Drop the fulltext index on spriden search last name if it already exists.
 --
+whenever sqlerror continue;
 declare
   index_does_not_exist EXCEPTION;
   pragma exception_init(index_does_not_exist,-01418);
@@ -41,10 +50,11 @@ exception
 	when index_does_not_exist then null;
 end;
 /
-
+whenever sqlerror exit rollback;
 --
 -- Drop the fulltext index on spriden search first name if it already exists.
 --
+whenever sqlerror continue;
 declare
   index_does_not_exist EXCEPTION;
   pragma exception_init(index_does_not_exist,-01418);
@@ -54,10 +64,11 @@ exception
 	when index_does_not_exist then null;
 end;
 /
-
+whenever sqlerror exit rollback;
 --
 -- Drop the domain index on spriden search middle name if it already exists.
 --
+whenever sqlerror continue;
 declare
   index_does_not_exist EXCEPTION;
   pragma exception_init(index_does_not_exist,-01418);
@@ -67,10 +78,11 @@ exception
 	when index_does_not_exist then null;
 end;
 /
-
+whenever sqlerror exit rollback;
 --
 -- Drop the domain index on spriden id if it already exists.
 --
+whenever sqlerror continue;
 declare
   index_does_not_exist EXCEPTION;
   pragma exception_init(index_does_not_exist,-01418);
@@ -80,10 +92,11 @@ exception
 	when index_does_not_exist then null;
 end;
 /
-
+whenever sqlerror exit rollback;
 --
 -- Drop the domain index on spriden change indicator name if it already exists.
 --
+whenever sqlerror continue;
 declare
   index_does_not_exist EXCEPTION;
   pragma exception_init(index_does_not_exist,-01418);
@@ -93,33 +106,38 @@ exception
 	when index_does_not_exist then null;
 end;
 /
-
+whenever sqlerror exit rollback;
 --
 -- Create the domain index on SPRIDEN_SEARCH_LAST_NAME.
 --
+whenever sqlerror continue;
 CREATE INDEX SPRIDEN_INDEX_LAST_NAME ON SPRIDEN (SPRIDEN_SEARCH_LAST_NAME) INDEXTYPE IS ctxsys.context parameters('STOPLIST STOPLIST_NAMESEARCH');
 
-
+whenever sqlerror exit rollback;
 --
 -- Create the domain index on SPRIDEN_SEARCH_FIRST_NAME.
 --
+whenever sqlerror continue;
 CREATE INDEX SPRIDEN_INDEX_FIRST_NAME ON SPRIDEN (SPRIDEN_SEARCH_FIRST_NAME) INDEXTYPE IS ctxsys.context parameters('STOPLIST STOPLIST_NAMESEARCH');
 
-
+whenever sqlerror exit rollback;
 --
 -- Create the domain index on SPRIDEN_SEARCH_MI.
 --
+whenever sqlerror continue;
 CREATE INDEX SPRIDEN_INDEX_MI ON SPRIDEN (SPRIDEN_SEARCH_MI) INDEXTYPE IS ctxsys.context parameters('STOPLIST STOPLIST_NAMESEARCH');
 
-
+whenever sqlerror exit rollback;
 --
 -- Create the domain index on SPRIDEN_ID.
 --
+whenever sqlerror continue;
 CREATE INDEX SPRIDEN_SEARCH_ID ON SPRIDEN (SPRIDEN_ID) INDEXTYPE IS ctxsys.context;
-
+whenever sqlerror exit rollback;
 --
 -- Create the function based index on SPRIDEN_CHANGE_IND.
 --
-
+whenever sqlerror continue;
 CREATE INDEX SPRIDEN_CHANGE_IND ON SPRIDEN (nvl(SPRIDEN_CHANGE_IND,'null'));
 
+whenever sqlerror exit rollback;
