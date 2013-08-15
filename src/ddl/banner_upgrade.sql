0ff95c31519b3b7c67f0584218d04f776cd53af1
Margy Hockett <margy.hockett@sungardhe.com>
2013-08-15 08:28:56 -0400

    remove DDL to create oracle full text indexes for idname search that will be part of the banner general 8.6.2 upgrade

--- src/ddl/banner_upgrade.sql
+++ src/ddl/banner_upgrade.sql
@@ -11,23 +11,10 @@ REM AUDIT TRAIL END
 REM
 set scan on echo on termout on;
 REM
-spool horizon_upgrade.lis
-REM
 connect dbeu_owner/&&dbeu_password
 REM
 start dbeu_ext_stu_bgp
 REM
-REM  oracle full text index
-connect saturn/&&saturn_password
-start spriden_fti_teardown.sql
-connect saturn/&&saturn_password
-start spriden_080602_01.sql
-start spriden_080602_02.sql
-start spriden_080602_03.sql
-start spriden_080602_04.sql
-start spriden_080602_05.sql
-start spriden_080602_06.sql
-start spbpers_080602_01.sql
 
 REM  Create package needed for view 
 connect baninst1/&&baninst1_password
