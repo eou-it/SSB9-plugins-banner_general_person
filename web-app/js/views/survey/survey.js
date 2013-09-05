8f5519ef15bea5d840cbe52f3dc5856429937583
sridhanya <sridhanya.ganapathi@ellucian.com>
2013-09-05 12:54:30 +0530

    Fix for HRU-4597

--- web-app/js/views/survey/survey.js
+++ web-app/js/views/survey/survey.js
@@ -1,20 +1,36 @@
 $(document).ready(function () {
-    $("#save-btn").click(function () {
-        if ($('#ethnicity input:checked').length > 1) {
-            $("#errorMessage").empty()
-            $("#errorMessage").append("You may only select one checkbox for Ethnicity")
-        }
-        else {
-            $("#errorMessage").empty()
-            var form = document.getElementById('surveyForm')
-            form.submit()
-        }
+    var notificationMessages = new Array();
+    var error = $.i18n.prop("survey.ethinicity.multiple.selection.invalid");
+
+    $('#chkEthn_1, #chkEthn_2').change(
+        function () {
+            if ($('#chkEthn_1').is(':checked') &&  $('#chkEthn_2').is(':checked')) {
+                $('#ethnicity').addClass("notification-error");
+                notificationMessages.push(error);
+                if (notificationMessages && notificationMessages.length > 0) {
+                    _.each(notificationMessages, function (message) {
+                        var n = new Notification({message:message, type:"error"});
+                        notifications.addNotification(n);
+                    });
+                }
+            } else {
+                notificationMessages.splice(notificationMessages.indexOf(error));
+                $('#ethnicity').removeClass("notification-error");
+                notifications.clearNotifications();
+                notificationCenter.removeNotification();
+            }
     });
+
+    $("#save-btn").click(function () {
+        var form = document.getElementById('surveyForm');
+        form.submit();
+    });
+
 
     $("#ask-me-later-btn").click(function () {
         /*window.location = "${createLink(controller: "survey", action: "completed")}"*/
-        var href = $(this).attr("data-endpoint")
-        window.location = href
+        var href = $(this).attr("data-endpoint");
+        window.location = href;
     });
 
-})
+});
