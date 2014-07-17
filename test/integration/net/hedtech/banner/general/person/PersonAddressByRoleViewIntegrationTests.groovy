9d1a54b5a265c0a0c7c0a07d97757eea2912b19b
Raji Venkatesan <raji.venkatesan@ellucian.com>
2014-07-16 21:57:56 -0400

    Adding domain for the view based on goradrl

---
+++ test/integration/net/hedtech/banner/general/person/PersonAddressByRoleViewIntegrationTests.groovy
@@ -0,0 +1,82 @@
+/*********************************************************************************
+  Copyright 2009-2013 Ellucian Company L.P. and its affiliates.
+ ********************************************************************************* */
+
+package net.hedtech.banner.general.person
+
+import net.hedtech.banner.general.person.view.PersonAddressByRoleView
+import net.hedtech.banner.testing.BaseIntegrationTestCase
+import org.springframework.dao.InvalidDataAccessResourceUsageException
+
+class PersonAddressByRoleViewIntegrationTests extends BaseIntegrationTestCase {
+
+
+	protected void setUp() {
+		formContext = ['GUAGMNU']
+		super.setUp()
+	}
+
+
+	protected void tearDown() {
+		super.tearDown()
+	}
+
+
+    void testCreatePersonAddressByRole() {
+        def pidm = PersonUtility.getPerson("710000006").pidm
+        def personAddress = newPersonAddress(pidm)
+        shouldFail(InvalidDataAccessResourceUsageException)   {
+            personAddress.save(failOnError: true, flush: true)
+        }
+    }
+
+
+    void testUpdatePersonAddressByRole() {
+        def pidm = PersonUtility.getPerson("511000008").pidm
+        def personAddress = PersonAddressByRoleView.findByPidm(pidm)
+
+        // Update domain values
+        personAddress.houseNumber = 5
+        shouldFail(InvalidDataAccessResourceUsageException)   {
+            personAddress.save(failOnError: true, flush: true)
+        }
+    }
+
+
+    void testDeletePersonAddressByRole() {
+        def pidm = PersonUtility.getPerson("511000008").pidm
+        def personAddress = PersonAddressByRoleView.findByPidm(pidm)
+
+        shouldFail(InvalidDataAccessResourceUsageException)   {
+            personAddress.delete(failOnError: true, flush: true)
+        }
+    }
+
+
+    void testFetchByPidmAndRole() {
+        def pidm = PersonUtility.getPerson("511000008").pidm
+        def employeeAddresses = PersonAddressByRoleView.fetchAddressesByPidmAndRole([pidm:pidm,role:'EMPLOYEE'])
+        assertTrue employeeAddresses.size() == 2
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private def newPersonAddress(pidm) {
+		def personAddress = new PersonAddressByRoleView(
+			pidm: pidm
+	    )
+		return personAddress
+	}
+
+
+}
