f0c68b2aa37261dfb81042277338ba19af5dd89a
Subodh Sharma <Subodh.Sharma@Ellucian.com>
2015-08-27 13:02:50 +0530

    BAPI-2932 - Implementation of Standardize date time format in V4 APIs and and the metadata removal

--- src/groovy/net/hedtech/banner/general/person/ldm/v1/Person.groovy
+++ src/groovy/net/hedtech/banner/general/person/ldm/v1/Person.groovy
@@ -22,8 +22,6 @@ class Person {
     List names = []
     List races = []
     List roles = []
-    def birthDate
-    def deadDate
 
     def Person(PersonBasicPersonBase person,
              def guid,
@@ -56,22 +54,6 @@ class Person {
         // PersonBasicPersonBase is optional, create blank unpersisted object if none exists.
         this.person = person ?: new PersonBasicPersonBase()
         this.metadata = new Metadata (this.person.dataOrigin)
-    }
-
-    def Person(PersonBasicPersonBase person,def birthDate,def deadDate) {
-        // PersonBasicPersonBase is optional, create blank unpersisted object if none exists.
-        this.person = person ?: new PersonBasicPersonBase()
-        this.metadata = new Metadata (this.person.dataOrigin)
-        this.birthDate = birthDate
-        this.deadDate = deadDate
-    }
-
-    def getBirthDate() {
-        return birthDate
-    }
-
-    def getDeadDate() {
-        return deadDate
     }
 
     def getSex() {
