b1e6e9bbfef1c099611afc4fcee29de5f0c1c84e
Craig Riddle <craig.riddle@ellucian.com>
2016-04-26 22:19:02 +0100

    attempting to fix ssn test that has been an issue for years. As requested by Sumeet and Kathleen

--- test/integration/net/hedtech/banner/general/person/PersonAdvancedSearchIDNameIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/person/PersonAdvancedSearchIDNameIntegrationTests.groovy
@@ -249,7 +249,7 @@ class PersonAdvancedSearchIDNameIntegrationTests extends BaseIntegrationTestCase
             //Step 1.
             // Client submits a search query to find an exact match
             // Search by SSN
-            def persons = personSearchService.personIdSearch("543-54-5432", filterData, pagingAndSortParams)
+            def persons = personSearchService.personIdSearch(personBase.ssn, filterData, pagingAndSortParams)
             assertNotNull persons
             assertTrue persons.size() >= 1
 
