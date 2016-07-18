a036924798e5758307abac0049aa43e8e56f6eea
Venu Thanneeru <venu.thanneeru@ellucian.com>
2016-07-18 12:37:19 +0530

    added generic methods for persons v6 filters

---
+++ grails-app/services/net/hedtech/banner/general/person/PersonAdvancedSearchViewService.groovy
@@ -0,0 +1,116 @@
+/*******************************************************************************
+ Copyright 2016 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+package net.hedtech.banner.general.person
+
+import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.general.common.GeneralValidationCommonConstants
+import net.hedtech.banner.general.person.view.PersonAdvancedSearchView
+import net.hedtech.banner.query.DynamicFinder
+import net.hedtech.banner.query.QueryBuilder
+import net.hedtech.banner.query.operators.Operators
+import net.hedtech.banner.service.ServiceBase
+
+/**
+ * The service class is used for read operations only, it does not support insert/update
+ */
+class PersonAdvancedSearchViewService extends ServiceBase {
+
+    boolean transactional = true
+
+    def preCreate(map) {
+        throwUnsupportedException()
+    }
+
+    def preUpdate(map) {
+        throwUnsupportedException()
+    }
+
+    def preDelete(map) {
+        throwUnsupportedException()
+    }
+
+    def throwUnsupportedException() {
+        throw new ApplicationException(PersonSearchView, "@@r1:unsupported.operation@@")
+    }
+
+    def fetchAllByCriteria(Map content, String sortField = null, String sortOrder = null, int max = 0, int offset = -1) {
+
+        def params = [:]
+        def criteria = []
+        def pagingAndSortParams = [:]
+
+        buildCriteria(content, params, criteria)
+
+        if (sortField) {
+            pagingAndSortParams.sort = sortField
+            if (["asc", "desc"].contains(sortOrder?.trim()?.toLowerCase())) {
+                pagingAndSortParams.order = sortOrder
+            }
+        }
+
+        if (max > 0) {
+            pagingAndSortParams.max = max
+        }
+        if (offset > -1) {
+            pagingAndSortParams.offset = offset
+        }
+
+        return finderByAll().find([params: params, criteria: criteria], pagingAndSortParams)
+    }
+
+    def countByCriteria(Map content) {
+        def params = [:]
+        def criteria = []
+
+        buildCriteria(content, params, criteria)
+        return finderByAll().count([params: params, criteria: criteria])
+    }
+
+
+    private void buildCriteria(Map content, LinkedHashMap params, ArrayList criteria) {
+
+        if (content.firstName) {
+            params.put("firstName", content.firstName?.trim().toLowerCase())
+            criteria.add([key: "firstName", binding: "firstName", operator: Operators.CONTAINS])
+        }
+
+        if (content.middleName) {
+            params.put("middleName", content.middleName?.trim().toLowerCase())
+            criteria.add([key: "middleName", binding: "middleName", operator: Operators.CONTAINS])
+        }
+
+        if (content.lastName) {
+            params.put("lastName", content.lastName?.trim().toLowerCase())
+            criteria.add([key: "lastName", binding: "lastName", operator: Operators.CONTAINS])
+        }
+
+        if (content.surnamePrefix) {
+            params.put("surnamePrefix", content.surnamePrefix?.trim().toLowerCase())
+            criteria.add([key: "surnamePrefix", binding: "surnamePrefix", operator: Operators.CONTAINS])
+        }
+
+        if (content.bannerId) {
+            params.put("bannerId", content.bannerId?.trim().toLowerCase())
+            criteria.add([key: "bannerId", binding: "bannerId", operator: Operators.EQUALS_IGNORE_CASE])
+        }
+
+        if (content.namePrefix) {
+            params.put("namePrefix", content.namePrefix?.trim().toLowerCase())
+            criteria.add([key: "namePrefix", binding: "namePrefix", operator: Operators.CONTAINS])
+        }
+
+        if (content.nameSuffix) {
+            params.put("nameSuffix", content.nameSuffix?.trim().toLowerCase())
+            criteria.add([key: "nameSuffix", binding: "nameSuffix", operator: Operators.CONTAINS])
+        }
+    }
+
+    def private finderByAll = {
+        def query = """FROM PersonAdvancedSearchView a
+                       where a.entityIndicator = 'P' and a.changeIndicator is null
+		            """
+        return new DynamicFinder(PersonAdvancedSearchView.class, query, "a")
+    }
+
+}
---
+++ test/integration/net/hedtech/banner/general/person/PersonAdvancedSearchViewServiceIntegrationTests.groovy
@@ -0,0 +1,222 @@
+/*********************************************************************************
+ Copyright 2016 Ellucian Company L.P. and its affiliates.
+ **********************************************************************************/
+
+package net.hedtech.banner.general.person
+
+import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.general.person.view.PersonAdvancedSearchView
+import net.hedtech.banner.testing.BaseIntegrationTestCase
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class PersonAdvancedSearchViewServiceIntegrationTests extends BaseIntegrationTestCase {
+
+    PersonAdvancedSearchViewService personAdvancedSearchViewService
+
+    @Before
+    public void setUp() {
+        formContext = ['GUAGMNU']
+        super.setUp()
+    }
+
+
+    @After
+    public void tearDown() {
+        super.tearDown()
+    }
+
+    @Test
+    void testFetchAllWithGuidByCriteria() {
+        String firstName
+        String lastName
+        String middleName
+        String surnamePrefix
+        String bannerId
+        String namePrefix
+        String nameSuffix
+
+        //with out pagination
+        List entities = personAdvancedSearchViewService.fetchAllByCriteria(params)
+        assertFalse entities.isEmpty()
+        entities.each {
+            PersonAdvancedSearchView personSearchView = it
+            assertNotNull personSearchView
+
+            //set filter value
+            if (personSearchView.bannerId && !bannerId) {
+                bannerId = personSearchView.bannerId
+            }
+
+            if (personSearchView.firstName && !firstName) {
+                firstName = personSearchView.firstName
+            }
+
+            if (personSearchView.middleName && !middleName) {
+                middleName = personSearchView.middleName
+            }
+
+            if (personSearchView.lastName && !lastName) {
+                lastName = personSearchView.lastName
+            }
+
+            if (personSearchView.surnamePrefix && !surnamePrefix) {
+                surnamePrefix = personSearchView.surnamePrefix
+            }
+
+            if (personSearchView.namePrefix && !namePrefix) {
+                namePrefix = personSearchView.namePrefix
+            }
+
+            if (personSearchView.nameSuffix && !nameSuffix) {
+                nameSuffix = personSearchView.nameSuffix
+            }
+
+        }
+        Long expectedCount = personAdvancedSearchViewService.countByCriteria(params)
+        assertEquals entities.size(), expectedCount
+
+        verifyFilterByBannerId(bannerId)
+        verifyFilterByFirstName(firstName)
+        verifyFilterByMiddleName(middleName)
+        verifyFilterByLastName(lastName)
+        verifyFilterBySurnamePrefix(surnamePrefix)
+        verifyFilterByNameSuffix(nameSuffix)
+        verifyFilterByNamePrefix(namePrefix)
+
+        //with pagination
+        params.clear()
+        entities = personAdvancedSearchViewService.fetchAllByCriteria(params, null, null, 1, 0)
+        assertFalse entities.isEmpty()
+        assertEquals 1, entities.size()
+        verifyFilterDataResponse(params, entities)
+    }
+
+    private void verifyFilterByBannerId(String bannerId) {
+        // bannerId filter
+        params.clear()
+        assertNotNull bannerId
+        params.bannerId = bannerId
+        def entities = personAdvancedSearchViewService.fetchAllByCriteria(params)
+        verifyFilterDataResponse(params, entities)
+        Long expectedCount = personAdvancedSearchViewService.countByCriteria(params)
+        assertEquals entities.size(), expectedCount
+    }
+
+    private void verifyFilterByFirstName(String firstName) {
+        // firstName filter
+        params.clear()
+        assertNotNull firstName
+        params.firstName = firstName
+        params.sort = 'lastName'
+        params.order = 'desc'
+        def entities = personAdvancedSearchViewService.fetchAllByCriteria(params, params.sort, params.order, 0, -1,)
+        verifyFilterDataResponse(params, entities)
+        Long expectedCount = personAdvancedSearchViewService.countByCriteria(params)
+        assertEquals entities.size(), expectedCount
+    }
+
+    private void verifyFilterByMiddleName(String middleName) {
+        // middleName filter
+        params.clear()
+        assertNotNull middleName
+        params.middleName = middleName
+        params.sort = 'firstName'
+        params.order = 'asc'
+        def entities = personAdvancedSearchViewService.fetchAllByCriteria(params, params.sort, params.order, 0, -1,)
+        verifyFilterDataResponse(params, entities)
+        Long expectedCount = personAdvancedSearchViewService.countByCriteria(params)
+        assertEquals entities.size(), expectedCount
+    }
+
+    private void verifyFilterByLastName(String lastName) {
+        // lastName filter
+        params.clear()
+        assertNotNull lastName
+        params.lastName = lastName
+        params.sort = 'firstName'
+        params.order = 'desc'
+        def entities = personAdvancedSearchViewService.fetchAllByCriteria(params, params.sort, params.order, 0, -1,)
+        verifyFilterDataResponse(params, entities)
+        Long expectedCount = personAdvancedSearchViewService.countByCriteria(params)
+        assertEquals entities.size(), expectedCount
+    }
+
+    private void verifyFilterBySurnamePrefix(String surnamePrefix) {
+        // surnamePrefix filter
+        params.clear()
+        assertNotNull surnamePrefix
+        params.surnamePrefix = surnamePrefix
+        params.sort = 'lastName'
+        params.order = 'asc'
+        def entities = personAdvancedSearchViewService.fetchAllByCriteria(params, params.sort, params.order, 0, -1,)
+        verifyFilterDataResponse(params, entities)
+        Long expectedCount = personAdvancedSearchViewService.countByCriteria(params)
+        assertEquals entities.size(), expectedCount
+    }
+
+    private void verifyFilterByNameSuffix(String nameSuffix) {
+        // nameSuffix filter
+        params.clear()
+        assertNotNull nameSuffix
+        params.nameSuffix = nameSuffix
+        params.sort = 'firstName'
+        params.order = 'asc'
+        def entities = personAdvancedSearchViewService.fetchAllByCriteria(params, params.sort, params.order, 0, -1,)
+        verifyFilterDataResponse(params, entities)
+        Long expectedCount = personAdvancedSearchViewService.countByCriteria(params)
+        assertEquals entities.size(), expectedCount
+    }
+
+    private void verifyFilterByNamePrefix(String namePrefix) {
+        // namePrefix filter
+        params.clear()
+        assertNotNull namePrefix
+        params.namePrefix = namePrefix
+        params.sort = 'firstName'
+        params.order = 'desc'
+        def entities = personAdvancedSearchViewService.fetchAllByCriteria(params, params.sort, params.order, 0, -1,)
+        verifyFilterDataResponse(params, entities)
+        Long expectedCount = personAdvancedSearchViewService.countByCriteria(params)
+        assertEquals entities.size(), expectedCount
+    }
+
+
+    private void verifyFilterDataResponse(params, entities) {
+        assertFalse entities.isEmpty()
+        entities.each {
+            PersonAdvancedSearchView personSearchView = it
+            assertNotNull personSearchView
+            if (params.bannerId) {
+                params.bannerId.contains(personSearchView.bannerId)
+            }
+
+            if (params.firstName) {
+                params.firstName.contains(personSearchView.firstName)
+            }
+
+            if (params.middleName) {
+                params.middleName.contains(personSearchView.middleName)
+            }
+
+            if (params.lastName) {
+                params.lastName.contains(personSearchView.lastName)
+            }
+
+            if (params.surnamePrefix) {
+                params.surnamePrefix.contains(personSearchView.surnamePrefix)
+            }
+
+            if (params.namePrefix) {
+                params.namePrefix.contains(personSearchView.namePrefix)
+            }
+
+            if (params.nameSuffix) {
+                params.nameSuffix.contains(personSearchView.nameSuffix)
+            }
+
+        }
+
+    }
+}
