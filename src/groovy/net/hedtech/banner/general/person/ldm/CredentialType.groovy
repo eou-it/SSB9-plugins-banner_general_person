73cbd73a5e1c86db420639a24555e0bab422bc6c
Venkat Koppisetty <venkat.koppisetty@ellucian.com>
2017-01-05 16:49:39 +0530

    Added v8 constants to CredentialType enum

--- src/groovy/net/hedtech/banner/general/person/ldm/CredentialType.groovy
+++ src/groovy/net/hedtech/banner/general/person/ldm/CredentialType.groovy
@@ -1,18 +1,19 @@
 /*******************************************************************************
- Copyright 2016 Ellucian Company L.P. and its affiliates.
+ Copyright 2016-2017 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 package net.hedtech.banner.general.person.ldm
 
 enum CredentialType {
 
-    SOCIAL_SECURITY_NUMBER([v3: "Social Security Number", v6: "ssn"]),
-    SOCIAL_INSURANCE_NUMBER([v3: "Social Insurance Number", v6: "sin"]),
-    BANNER_ID([v3: "Banner ID", v6: "bannerId"]),
-    COLLEAGUE_PERSON_ID([v3: "Colleague Person ID", v6: "colleaguePersonId"]),
-    ELEVATE_ID([v3: "Elevate ID", v6: "elevateId"]),
-    BANNER_SOURCED_ID([v3: "Banner Sourced ID", v6: "bannerSourcedId"]),
-    BANNER_USER_NAME([v3: "Banner User Name", v6: "bannerUserName"]),
-    BANNER_UDC_ID([v3: "Banner UDC ID", v6: "bannerUdcId"])
+    SOCIAL_SECURITY_NUMBER([v3: "Social Security Number", v6: "ssn", v8: "ssn"]),
+    SOCIAL_INSURANCE_NUMBER([v3: "Social Insurance Number", v6: "sin", v8: "sin"]),
+    BANNER_ID([v3: "Banner ID", v6: "bannerId", v8: "bannerId"]),
+    COLLEAGUE_PERSON_ID([v3: "Colleague Person ID", v6: "colleaguePersonId", v8: "colleaguePersonId"]),
+    ELEVATE_ID([v3: "Elevate ID", v6: "elevateId", v8: "elevateId"]),
+    BANNER_SOURCED_ID([v3: "Banner Sourced ID", v6: "bannerSourcedId", v8: "bannerSourcedId"]),
+    BANNER_USER_NAME([v3: "Banner User Name", v6: "bannerUserName", v8: "bannerUserName"]),
+    BANNER_UDC_ID([v3: "Banner UDC ID", v6: "bannerUdcId", v8: "bannerUdcId"]),
+    COLLEAGUE_USER_NAME([v8: "colleagueUserName"])
 
     private final Map<String, String> versionToEnumMap
 
@@ -26,15 +27,8 @@ enum CredentialType {
         return versionToEnumMap
     }
 
-    /**
-     * Given "bannerSourcedId" and "v6" returns corresponding enum BANNER_SOURCED_ID.
-     * This is useful in "create" and "update" operations to validate the input string.
-     *
-     * @param value
-     * @param version
-     * @return
-     */
-    public static CredentialType getByString(String value, String version) {
+
+    public static CredentialType getByDataModelValue(String value, String version) {
         if (value) {
             Iterator itr = CredentialType.values().iterator()
             while (itr.hasNext()) {
