883e223008dc4a7e0d2c650a8d8929ffd3612824
rajeshk <rajesh.kubireddi@ellucian.com>
2020-10-30 10:10:57 +0530

    code changes for GRDSbName and program

---
+++ grails-app/services/net/hedtech/banner/general/person/PersonCommentsService.groovy
@@ -0,0 +1,23 @@
+/** *****************************************************************************
+ Copyright 2020 Ellucian Company L.P. and its affiliates.
+ ****************************************************************************** */
+package net.hedtech.banner.general.person
+
+import grails.gorm.transactions.Transactional
+import net.hedtech.banner.service.ServiceBase
+
+@Transactional
+class PersonCommentsService extends ServiceBase {
+
+    def preCreate( domainModelOrMap ) {
+
+    }
+
+    def preUpdate( domainModelOrMap ) {
+
+    }
+
+    def preDelete(domainModelOrMap) {
+
+    }
+}
